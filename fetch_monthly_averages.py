d2aafb7a21881ccc1b430455dd0bc1a5ac282ab5
Arto Santala <crystoll@gmail.com>
2022-01-03 23:52:12 +0200

    Added also monthly averages (some cleanup to do on head/tail values)

--- fetch_monthly_averages.py
+++ fetch_monthly_averages.py
@@ -5,19 +5,24 @@ from pycoingecko import CoinGeckoAPI
 cg = CoinGeckoAPI()
 id = 'bitcoin'
 
-def get_eur_prices_as_dataframe(id='bitcoin',days=365):
+def get_eur_monthly_avg_prices_as_dataframe(id='bitcoin',days=365):
     market_chart = cg.get_coin_market_chart_by_id(id=id, vs_currency='eur', interval='daily', days=days)
     df = pd.DataFrame(market_chart['prices'][:-1], columns=['date', 'price'])
     df['date']=(pd.to_datetime(df['date'],unit='ms')).dt.strftime('%d.%m.%Y')
+    # Calculate monthly averages
+    df['month'] = df['date'].str.split('.').str[1]
+    df['year'] = df['date'].str.split('.').str[2]
+    df['month_year'] = df['year'] + '-' + df['month']
+    df['month_year'] = pd.to_datetime(df['month_year'], format='%Y-%m')
+    df = df.groupby(['month_year']).mean()
+    df = df.reset_index()
     return df
 
-btc_df = get_eur_prices_as_dataframe('bitcoin')
+btc_df = get_eur_monthly_avg_prices_as_dataframe('bitcoin')
+btc_df.to_csv('bitcoin_monthly_averages.csv', index=False, header=True, quoting=csv.QUOTE_NONNUMERIC)
 
-# TODO
 
 
-# btc_df.to_csv('bitcoin_market_chart.csv', index=False, header=True, quoting=csv.QUOTE_NONNUMERIC)
-
-# erg_df = get_eur_prices_as_dataframe('ergo')
-# erg_df.to_csv('ergo_market_chart.csv', index=False, header=True, quoting=csv.QUOTE_NONNUMERIC)
+erg_df = get_eur_monthly_avg_prices_as_dataframe('ergo')
+erg_df.to_csv('ergo_monthly_averages.csv', index=False, header=True, quoting=csv.QUOTE_NONNUMERIC)
 
